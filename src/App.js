bca0a9823b71f0ba936ff42b493f1720ec8c570b
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2021-01-26 22:28:25 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='menu'>
+        <h3>Menu</h3>
+        <h3>Meds</h3>
+        <h3>Notes</h3>
+        <h3>Calendar</h3>
+      </div>
+      <div className='content'>
+        <h1>Dashboard</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
